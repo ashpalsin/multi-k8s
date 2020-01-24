607669f149c42105ade2a4aac8eabe835709e678
Ashpal Singh <ashpal.singh@msbdocs.com>
2020-01-24 12:26:10 +0530

    deployment save

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t ashpalsin/multi-client:latest -t ashpalsin/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t ashpalsin/multi-server:latest -t ashpalsin/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t ashpalsin/multi-worker:latest -t ashpalsin/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push ashpalsin/multi-client:latest
+docker push ashpalsin/multi-server:latest
+docker push ashpalsin/multi-worker:latest
+
+docker push ashpalsin/multi-client:$SHA
+docker push ashpalsin/multi-server:$SHA
+docker push ashpalsin/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=ashpalsin/multi-server:$SHA
+kubectl set image deployments/client-deployment server=ashpalsin/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=ashpalsin/multi-worker:$SHA
