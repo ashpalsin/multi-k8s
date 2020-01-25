6221e1007edfb8d740ba6da0a3e6070230863619
Ashpal Singh <ashpal.singh@msbdocs.com>
2020-01-25 18:10:12 +0530

    new set 9

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t ashpalsin/multi-client:latest -t ashpalsin/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t ashpalsin/multi-server:latest -t ashpalsin/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t ashpalsin/multi-worker:latest -t ashpalsin/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t ashpalsin/multi-client:latest -t ashpalsin/multi-client:$SHA -f ./client/Dockerfile ./client/
+docker build -t ashpalsin/multi-server:latest -t ashpalsin/multi-server:$SHA -f ./server/Dockerfile ./server/
+docker build -t ashpalsin/multi-worker:latest -t ashpalsin/multi-worker:$SHA -f ./worker/Dockerfile ./worker/
 docker push ashpalsin/multi-client:latest
 docker push ashpalsin/multi-server:latest
 docker push ashpalsin/multi-worker:latest
