542388a6ec97a87c01b082c87a80415ffddab875
Ashpal Singh <ashpal.singh@msbdocs.com>
2020-01-25 17:36:53 +0530

    new set 6

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,12 @@
 docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-
 docker push ashpalsin/multi-client:latest
 docker push ashpalsin/multi-server:latest
 docker push ashpalsin/multi-worker:latest
-
 docker push ashpalsin/multi-client:$SHA
 docker push ashpalsin/multi-server:$SHA
 docker push ashpalsin/multi-worker:$SHA
-
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=ashpalsin/multi-server:$SHA
 kubectl set image deployments/client-deployment client=ashpalsin/multi-client:$SHA
