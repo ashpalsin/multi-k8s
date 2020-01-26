c66567a3c48a74427b4ff5d08eb2923e88fabd6e
Ashpal Singh <ashpal.singh@msbdocs.com>
2020-01-26 11:11:31 +0530

    new set 20

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,13 @@
 echo $SHA
-chmod 0755 client
-chmod 0755 server
-chmod 0755 worker
-sudo docker build -t ashpalsin/multi-client:latest -t ashpalsin/multi-client:$SHA -f ./client/Dockerfile.txt ./client
-sudo docker build -t ashpalsin/multi-server:latest -t ashpalsin/multi-server:$SHA -f ./server/Dockerfile.txt ./server
-sudo docker build -t ashpalsin/multi-worker:latest -t ashpalsin/multi-worker:$SHA -f ./worker/Dockerfile.txt ./worker
-sudo docker push ashpalsin/multi-client:latest
-sudo docker push ashpalsin/multi-server:latest
-sudo docker push ashpalsin/multi-worker:latest
-sudo docker push ashpalsin/multi-client:$SHA
-sudo docker push ashpalsin/multi-server:$SHA
-sudo docker push ashpalsin/multi-worker:$SHA
+docker build -t ashpalsin/multi-client:latest -t ashpalsin/multi-client:$SHA -f ./client/Dockerfile.txt https://github.com/ashpalsin/multi-k8s/tree/master/client
+docker build -t ashpalsin/multi-server:latest -t ashpalsin/multi-server:$SHA -f ./server/Dockerfile.txt https://github.com/ashpalsin/multi-k8s/tree/master/server
+docker build -t ashpalsin/multi-worker:latest -t ashpalsin/multi-worker:$SHA -f ./worker/Dockerfile.txt https://github.com/ashpalsin/multi-k8s/tree/master/worker
+docker push ashpalsin/multi-client:latest
+docker push ashpalsin/multi-server:latest
+docker push ashpalsin/multi-worker:latest
+docker push ashpalsin/multi-client:$SHA
+docker push ashpalsin/multi-server:$SHA
+docker push ashpalsin/multi-worker:$SHA
 kubectl apply -f K8s
 kubectl set image deployments/server-deployment server=ashpalsin/multi-server:$SHA
 kubectl set image deployments/client-deployment client=ashpalsin/multi-client:$SHA
